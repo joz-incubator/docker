a613beda21fb04b5998603d239d8ccf562e143d2
joz-incubator <gudauskas.juozas@gmail.com>
2025-08-27 16:49:44 +0200

    Update startup_vm1.sh

--- scripts/startup_vm1.sh
+++ scripts/startup_vm1.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-apt update && apt install -y docker.io
+
+# Update OS and install Docker
+apt update && apt full-upgrade -y
+apt install -y docker.io
+
+# Ensure Docker is up to date
+apt install --only-upgrade -y docker.io
+
+# Start Docker
 systemctl start docker
 
 # Create ipvlan network
-docker network create -d ipvlan   --subnet=192.168.100.0/24   --gateway=192.168.100.1   -o parent=eth0 ipvlan-net
+docker network create -d ipvlan \
+  --subnet=192.168.100.0/24 \  # or 192.168.200.0/24 for vm2
+  --gateway=192.168.100.1 \    # or 192.168.200.1 for vm2
+  -o parent=eth0 ipvlan-net
 
 # Run container with static IP
-docker run -d --net=ipvlan-net --ip=192.168.100.10 nginx
+docker run -d --net=ipvlan-net --ip=192.168.100.10 nginx  # or 192.168.200.10 for vm2
