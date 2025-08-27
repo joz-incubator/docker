6236f35310da66317165ac1ea8a50517c6aa8408
joz-incubator <gudauskas.juozas@gmail.com>
2025-08-27 20:18:03 +0200

    Update variables.tf

--- modules/subnet/variables.tf
+++ modules/subnet/variables.tf
@@ -1,1 +1,33 @@
+variable "name" {
+  description = "Name of the VPC"
+  type        = string
+}
 
+variable "cidr" {
+  description = "CIDR range for the subnet"
+  type        = string
+}
+
+variable "region" {
+  description = "Region for the subnet and router"
+  type        = string
+}
+
+variable "cidrdock" {
+  description = "docker ip range"
+  type = string
+}
+
+variable "dock_range_name" {
+  type = string
+}
+
+variable "iap_firewall_name" {
+  description = "Name of the IAP SSH firewall rule"
+  type        = string
+}
+
+variable "egress_firewall_name" {
+  description = "Name of the egress 443 firewall rule"
+  type        = string
+}
