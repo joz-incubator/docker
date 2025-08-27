1213cad1ba30cb3e5ed00059bef2f69876786cd1
joz-incubator <gudauskas.juozas@gmail.com>
2025-08-27 15:53:58 +0200

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,4 +1,17 @@
+variable "project_id" {
+  description = "GCP project ID"
+  type        = string
+  default     = "he-prod-itinfra-incubator"
+}
 
-variable "project_id" {}
-variable "region" {}
-variable "zone" {}
+variable "region" {
+  description = "GCP region"
+  type        = string
+  default     = "europe-west6" # Zurich region
+}
+
+variable "zone" {
+  description = "GCP zone"
+  type        = string
+  default     = "europe-west6-a"
+}
