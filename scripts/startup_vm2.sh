8a86882cf19a425c295da76015282ca61c528e78
joz-incubator <gudauskas.juozas@gmail.com>
2025-08-28 00:05:45 +0200

    Update startup_vm2.sh

--- scripts/startup_vm2.sh
+++ scripts/startup_vm2.sh
@@ -10,3 +10,11 @@ apt install --only-upgrade -y docker.io
 # Start Docker
 systemctl start docker
 
+# Detect primary network interface (excluding loopback and docker interfaces)
+PARENT_IF=$(ip -o -4 route show to default | awk '{print $5}')
+
+# Create ipvlan network
+docker network create -d ipvlan --subnet=192.168.200.0/24 -o parent=$PARENT_IF -o ipvlan_mode=l3 ipvlan-net
+
+# Run container with static IP
+docker run -d --net=ipvlan-net --ip=192.168.200.10 nginx
