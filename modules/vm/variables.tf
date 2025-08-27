90cb6269abb74befdeb8c9a0c78b51c6b02ea9c9
joz-incubator <gudauskas.juozas@gmail.com>
2025-08-27 15:59:09 +0200

    Update variables.tf

--- modules/vm/variables.tf
+++ modules/vm/variables.tf
@@ -1,6 +1,24 @@
+variable "name" {
+  description = "Name of the VM instance"
+  type        = string
+}
 
-variable "name" {}
-variable "network_name" {}
-variable "subnet_name" {}
-variable "zone" {}
-variable "startup_script" {}
+variable "network_name" {
+  description = "Name of the VPC network"
+  type        = string
+}
+
+variable "subnet_name" {
+  description = "Name of the subnet"
+  type        = string
+}
+
+variable "zone" {
+  description = "Zone where the VM will be deployed"
+  type        = string
+}
+
+variable "startup_script" {
+  description = "Startup script to configure the VM"
+  type        = string
+}
