9a799dcfaf28021c7f862d945708dd93c4ae7adc
joz-incubator <gudauskas.juozas@gmail.com>
2025-08-27 21:32:41 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -31,6 +31,31 @@ resource "google_compute_firewall" "egress443" {
   destination_ranges = ["0.0.0.0/0"]
 }
 
+
+resource "google_compute_router" "router" {
+  name    = "${var.name}-router"
+  network = var.network_self_link
+  region  = var.region
+}
+
+resource "google_compute_router_nat" "nat" {
+  name                               = "${var.name}-nat"
+  router                             = google_compute_router.router.name
+  region                             = var.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
+
+  enable_endpoint_independent_mapping = true
+
+  min_ports_per_vm = 64
+
+  log_config {
+    enable = true
+    filter = "ALL"
+  }
+}
+
+
 module "subnet1" {
   source = "./modules/subnet"
   name = "subnet1"
