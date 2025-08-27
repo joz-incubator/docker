348f9a016da55a174e9393a8f6f22158f4e10bc6
joz-incubator <gudauskas.juozas@gmail.com>
2025-08-27 15:57:56 +0200

    Update variables.tf

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,3 +1,17 @@
-variable "name" {}
-variable "cidr" {}
-variable "region" {}
+variable "name" {
+  description = "Name of the VPC"
+  type        = string
+  default     = "vpc-xxx"
+}
+
+variable "cidr" {
+  description = "CIDR range for the subnet"
+  type        = string
+  default     = "192.168.1.0/24"
+}
+
+variable "region" {
+  description = "Region for the subnet and router"
+  type        = string
+  default     = "europe-west6"
+}
