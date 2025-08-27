d473c705ccd7f56a64fdecf94a35308bcbc8702e
joz-incubator <gudauskas.juozas@gmail.com>
2025-08-27 16:51:40 +0200

    Update startup_vm2.sh

--- scripts/startup_vm2.sh
+++ scripts/startup_vm2.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-apt update && apt install -y docker.io
+
+# Update OS and install Docker
+apt update && apt full-upgrade -y
+apt install -y docker.io
+
+# Ensure Docker is up to date
+apt install --only-upgrade -y docker.io
+
+# Start Docker
 systemctl start docker
 
 # Create ipvlan network
