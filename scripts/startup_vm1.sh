690d2d896d63dd13cd684d33f5d494f517d7cba8
joz-incubator <gudauskas.juozas@gmail.com>
2025-08-27 15:11:09 +0200

    Create startup_vm1.sh

---
+++ scripts/startup_vm1.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt update && apt install -y docker.io
+systemctl start docker
+
+# Create ipvlan network
+docker network create -d ipvlan   --subnet=192.168.100.0/24   --gateway=192.168.100.1   -o parent=eth0 ipvlan-net
+
+# Run container with static IP
+docker run -d --net=ipvlan-net --ip=192.168.100.10 nginx
