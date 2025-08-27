ce7a713242643c398c173c7811c3720be52a9803
joz-incubator <gudauskas.juozas@gmail.com>
2025-08-27 16:26:27 +0200

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -10,6 +10,12 @@ resource "google_compute_instance" "vm" {
     }
   }
 
+    shielded_instance_config {
+      enable_integrity_monitoring = true
+      enable_secure_boot          = true
+      enable_vtpm                 = true
+  }
+
   network_interface {
     network    = var.network_name
     subnetwork = var.subnet_name
