7ef6aae36dab392f5e3006027e4ee4ffd8670789
joz-incubator <gudauskas.juozas@gmail.com>
2025-08-28 13:11:32 +0200

    Update startup_vm1.sh

--- scripts/startup_vm1.sh
+++ scripts/startup_vm1.sh
@@ -13,8 +13,20 @@ systemctl start docker
 # Detect primary network interface (excluding loopback and docker interfaces)
 PARENT_IF=$(ip -o -4 route show to default | awk '{print $5}')
 
+# Enable forwarding
+sysctl -w net.ipv4.ip_forward=1
+
+# Disable strict reverse-path filtering
+sysctl -w net.ipv4.conf.all.rp_filter=0
+sysctl -w net.ipv4.conf.default.rp_filter=0
+
+# create interface and gateway for local containers
+ip link add ipvlan link $PARENT_IF type ipvlan mode l3
+ip addr add 192.168.100.1/24 dev ipvlan 
+ip link set ipvlan up
+
 # Create ipvlan network
-docker network create -d ipvlan --subnet=192.168.100.0/24 -o parent=$PARENT_IF -o ipvlan_mode=l3 ipvlan-net
+docker network create -d ipvlan --subnet=192.168.100.0/24 --gateway=192.168.100.1  -o ipvlan_mode=l3 -o parent=$PARENT_IF ipvlan-net
 
 # Run container with static IP
 docker run -d --net=ipvlan-net --ip=192.168.100.10 nginx
